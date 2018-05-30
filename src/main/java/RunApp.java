61947b71d2ea1ba7acddf8912c8707d71d027a2d
invincible-akshay <invincible-akshay@users.noreply.github.com>
2018-05-30 22:18:58 +0530

    Initial Commit

---
+++ src/main/java/RunApp.java
@@ -0,0 +1,8 @@
+import com.adn.utils.EncryptDecryptService;
+
+public class RunApp {
+    public static void main(String[] args) {
+        EncryptDecryptService encryptDecryptService = new EncryptDecryptService();
+
+    }
+}
