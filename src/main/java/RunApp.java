59d445d584e260a9cd0ec2c99ea9b3dd61b5bdf7
invincible-akshay <invincible-akshay@users.noreply.github.com>
2018-05-31 00:10:53 +0530

    Learning Spring Boot

--- src/main/java/RunApp.java
+++
@@ -1,8 +0,0 @@
-import com.adn.utils.EncryptDecryptService;
-
-public class RunApp {
-    public static void main(String[] args) {
-        EncryptDecryptService encryptDecryptService = new EncryptDecryptService();
-
-    }
-}
